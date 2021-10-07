0fdeb73a60b4f4bd919d3ab6e9ec6166e13835d3
yume-22 <84458045+yume-22@users.noreply.github.com>
2021-10-07 23:48:48 +0900

    Add files via upload

--- jQuely/menu.js
+++ jQuely/menu.js
@@ -1,15 +1,9 @@
-$('.btn-trigger').on('click', function(){
-    $(this).toggleClass('active');
-    if ($(this).hasClass('active')){
-        $('nav').css({
-            'visibility': 'visible',
-            'opacity': 1
-        });
-    } else {
-        $('nav').css({
-            'visibility': 'hidden',
-            'opacity': 0
-        });
-    }
-    return false;
+$(function(){
+
+  $(".menu").hide();
+
+  $(".navButton").click( function(){
+    $(".menu").slideToggle();
+  });
+
 });
